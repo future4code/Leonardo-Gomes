d5db9776f593d71e5c0ff8a427c90767bc1e6f13
LeonardoGomesX <leo.sgomes60@gmail.com>
2021-11-28 13:29:37 -0300

    To-Do lista

--- semana16/to-do/src/index.ts
+++ semana16/to-do/src/index.ts
@@ -24,41 +24,164 @@ const connection = knex({
     }
 });
 
-const createUser = async (name: string , nickname: string, email: string): Promise<any> => {
-    await connection("Users")
-    .insert({
-        name: name,
-        nickname: nickname,
-        email: email
-    })
-};
 
-// POST CRIAR USUARIO 
 
-app.post("/Users", async (req: Response, res: Response) => {
-    try{
-        const {name, nickname, email} = req.body
-        
-        if(!name || !nickname || !email) {
+// Tabela de usuários
+const createUser = async (name: string, nickname: string, email: string): Promise<any> => {
+    await connection("TodoListUser")
+        .insert({
+            name: name,
+            nickname: nickname,
+            email: email
+        })
+}
+
+// CRIAR USUARIO
+
+app.post("/user", async (req: Request, res: Response) => {
+    try {
+        const { name, nickname, email } = req.body
+
+        if (!name || !nickname || !email) {
             res.statusCode = 406
-            throw new Error("Digite todos os campos")
+            throw new Error("Digite todos os campos solicitados.")
         }
-        
+
+
         await createUser(name, nickname, email)
 
         res.status(200).send("Usuário criado com sucesso!")
+
+    } catch (error: any) {
+        res.status(400).send(error.message)
+    }
+})
+
+
+// PEGAR USUARIO BY ID
+
+const getActorById = async (id: number): Promise<any> => {
+    const result = await connection("TodoListUser")
+        .select("id", "nickname")
+        .where("id", id)
+
+    return result
+}
+
+// (async () => {
+//     console.log(await getActorById(8))
+// })()
+
+app.get("/user/:id", async (req: Request, res:Response) => {
+    try {
+        const id: any = req.params.id
+
+        const user = await getActorById(id)
+        if (user.length === 0) {
+            res.statusCode = 404
+            throw new Error("Usuário não encontado.")
+        }
+
+        res.status(200).send(user)
+    } catch (error:any) {
+        res.status(res.statusCode).send(error.message)
+    }
+})
+
+
+// EDITAR USUARIO
+
+const editUser = async (id: number, name: string, nickname: string): Promise<any> => {
+    await connection("TodoListUser")
+    .update({
+        name: name,
+        nickname: nickname
+    })
+    .where("id", id)
+}
+
+app.put("/user/edit/:id", async (req: Request, res:Response) => {
+    try {
+        const id: any = req.params.id
+        const {name, nickname} = req.body
+        if (!name || !nickname) {
+            res.statusCode = 422
+            throw new Error("Todos os dados precisam ser preenchidos.")
+        }
+
+        await editUser(id, name, nickname)
+        res.status(200).send("Usuário atualizado!")
     } catch (error:any) {
         res.status(400).send(error.message)
     }
 })
 
 
-// PEGAR USUARIO POR ID
+// CRIAR TAREFA
+
+const createTask = async (title: string, description: string, limit_date: Date, creator_user_id: number): Promise<any> => {
+    await connection("TodoListTask")
+        .insert({
+            title: title,
+            description: description,
+            limit_date: limit_date,
+            creator_user_id: creator_user_id
+        })
+}
+
+app.post("/task", async (req: Request, res: Response) => {
+    try {
+        const {title, description, limit_date, creator_user_id} = req.body
+
+        const [day, month, year] = limit_date.split("/")
+        const limitDate: Date = new Date(`${year}-${month}-${day}`)
+
+        if (!title || !description || !limit_date || !creator_user_id) {
+            res.statusCode = 422
+            throw new Error("Todos os dados precisam ser preenchidos.")
+        }
+
+        await createTask(title, description, limitDate, creator_user_id)
+
+        res.status(200).send("Tarefa criada com sucesso!")
+    } catch (error:any) {
+        res.status(res.statusCode).send(error.message)
+    }
+})
+
+// PEGAR TAREFA BY ID
+
+const getTaskById = async (id: number): Promise<any> => {
+    const result = await connection("TodoListUser")
+        .innerJoin("TodoListTask", "TodoListUser.id", "TodoListTask.creator_user_id") 
+        .select("TodoListTask.id", "title", "description", "status", "limit_date", "creator_user_id", "nickname")
+        .where("TodoListTask.id", id)
+
+    return result
+}
+
+app.get("/task/:id", async (req: Request, res: Response) => {
+    try {
+        const id: any = req.params.id
 
 
+        const task = await getTaskById(id)
 
+        let formatedDate = task[0].limit_date
+        let realDate = ((formatedDate.getDate() )) + "/" + ((formatedDate.getMonth() + 1)) + "/" + formatedDate.getFullYear(); 
 
+        const formatedTask = {...task[0], limit_date: realDate}
 
+        if (task.length === 0) {
+            res.statusCode = 404
+            throw new Error("Tarefa não encontada.")
+        }
+
+        res.status(200).send(formatedTask)
+    } catch (error:any) {
+        res.status(res.statusCode).send(error.message)
+    }    
+})
 
 
 const server = app.listen(process.env.PORT || 3003, () => {
